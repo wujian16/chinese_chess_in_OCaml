17c48fce8fb8d4c058dc438484e7ddd91bddf5c7
Jian Wu <jw926@cornell.edu>
2015-11-09 18:06:40 -0500

    update the architecture

--- src/main.ml
+++
@@ -1,5 +0,0 @@
-(* The main file for REPL*
- * including printing the chess board
- * printing the information needed for player
- * starting some dialogue with player
- * starting the game, ending the game, etc)
