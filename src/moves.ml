e78eb11ec5d57fa800d18ae1bb98f10f8de3711a
CandiceW <yw654@cornell.edu>
2015-11-22 22:21:21 -0500

    added advisor and genearl

--- src/moves.ml
+++ src/moves.ml
@@ -10,8 +10,20 @@ type step={start:position; destination: position; piece_captured: piece option}
 
   let update_catch_piece = TODO
 
+  let check_position_eat (b: board) (p:position) : (piece option) =
+    let
+    match   with
+    | patt -> expr
+    | _ -> expr2
+
   let in_bound ((x,y):position) : bool=
-    x>=1 && x<=9 && y>=1 && y <=9
+    x>=1 && x<=9 && y>=1 && y <=10
+
+  let in_square (pc:piece) ((x,y): position) : bool =
+    match pc.tea with
+    | true  -> x>=4 &&x<=6 && y>=1 && y<=3
+    | false -> x>=4 && x <= 6 && y>=8 && y<=10
+
 
 
   (*generate all the steps of a Rook on board with position p *)
@@ -25,8 +37,9 @@ type step={start:position; destination: position; piece_captured: piece option}
       match  (check_position b curr_position), (in_bound curr_position) with
       | None, true -> result := {start = (x,y); destination = curr_position ;
           piece_captured = None}::(!result); loop_forward (x, y+1)
-      | sth ,true -> result := {start = (x,y); destination = curr_position;
-          piece_captured = sth}::(!result)
+      | Some sth ,true -> if sth.team <> pc.team then
+          result := {start = (x,y); destination = curr_position;
+          piece_captured = sth}::(!result) else ()
       | _ , _ -> ()
     end
       in
@@ -36,8 +49,9 @@ type step={start:position; destination: position; piece_captured: piece option}
       match (check_position b curr_position), (in_bound curr_position )with
       | None, true-> result := {start = (x, y); destination = curr_position;
         piece_captured = None}::(!result); loop_back (x, y-1)
-      | sth ,true -> result := {start = (x, y); destination = curr_position;
-        piece_captured = sth}::(!result)
+      | Some sth ,true -> if sth.team <> pc.team then
+          result := {start = (x, y); destination = curr_position;
+        piece_captured = sth}::(!result)  else ()
       | _ , _ -> ()
     end
       in
@@ -47,8 +61,9 @@ type step={start:position; destination: position; piece_captured: piece option}
         match (check_position b curr_position), (in_bound curr_position )with
         | None, true -> result:= {start = (x, y); destination = curr_position;
           piece_captured = None}::(!result); loop_left (x-1, y)
-        | sth ,true ->  result := {start = (x, y); destination = curr_position;
-          piece_captured = sth}::(!result)
+        | Some sth ,true -> if sth.team <> pc.team then
+             result := {start = (x, y); destination = curr_position;
+          piece_captured = sth}::(!result) else ()
         | _ , _ -> ()
     end
 
@@ -58,8 +73,9 @@ type step={start:position; destination: position; piece_captured: piece option}
         match (check_position b curr_position), (in_bound curr_position )with
         | None, true -> result:= {start = (x, y); destination = curr_position;
           piece_captured = None}::(!result); loop_right (x+1, y)
-        | sth ,true ->  result := {start = (x, y); destination = curr_position;
-          piece_captured = sth}::(!result)
+        | Some sth ,true -> if sth.team <> pc.team then
+             result := {start = (x, y); destination = curr_position;
+          piece_captured = sth}::(!result) else ()
        | _ , _ -> ()
     end
 
@@ -97,22 +113,30 @@ type step={start:position; destination: position; piece_captured: piece option}
   in  result
   end*)
 (*move *)
-  let move_sodier (b:board) (pc:piece) ((x,y): position) : step list =
+  let move_soldier (b:board) (pc:piece) ((x,y): position) : step list =
     (*red piece in its own part: row 0-5*)
     match pc.team, y<=5 with
      (*red piece, in river side*)
      | true,  true -> [{start= (x,y); destination = (x, y+1); piece_captured =
       (check_position b (x, y+1))}]
      (*red piece, other side of river*)
-     | true, false -> let raw_pos = [(x+1, y), (x-1, y), (x, y+1) ] in
-     List.flatten (List.map (fun p -> if in_bound p then [{start= (x,y); destination = p;
+     | true, false -> let raw_pos = [(x+1, y; (x-1, y); (x, y+1) ] in
+     List.flatten (List.map (fun p -> if (in_bound p) && (
+        match piece_captured b p with
+        | Some sth-> sth.team <> pc.team
+        | None -> true
+     ) then [{start= (x,y); destination = p;
         piece_captured = (check_position b p)}] else []) raw_pos )
      (*black piece, own side*)
      | false, true -> [{start= (x,y); destination = (x, y-1); piece_captured =
       (check_position b (x, y+1))}]
      (*black piece, other side*)
-     | false, false-> let raw_pos = [(x+1, y), (x-1, y), (x, y-1) ] in
-     List.flatten (List.map (fun p -> if in_bound p then [{start= (x,y); destination = p;
+     | false, false-> let raw_pos = [(x+1, y); (x-1, y); (x, y-1) ] in
+     List.flatten (List.map (fun p -> if (in_bound p) && (
+        match piece_captured b p with
+        | Some sth-> sth.team <> pc.team
+        | None -> true
+     ) then [{start= (x,y); destination = p;
         piece_captured = (check_position b p)}] else []) raw_pos )
 
 
@@ -149,92 +173,46 @@ type step={start:position; destination: position; piece_captured: piece option}
       end
 *)
 
-  let move_horse = TODO
+  let move_horse =
+    (*red piece in its own part: row 0-5*)
+     let raw_hori_pos = [(x+2, y+1); (x+2, y-1); (x-2,y+1); (x-2,y-1)] in
+     let hori_pos = List.flatten (List.map (fun p -> if (in_bound p) && (
+        match piece_captured b p with
+        | Some sth-> sth.team <> pc.team
+        | None -> true
+     ) && ( check_position ((p.x + x)/2 , y) = None) then [{start= (x,y); destination = p;
+        piece_captured = (check_position b p)}] else []) raw_hori pos
+   in let vert_raw_pos = [(x+1, y+2) ; (x+1,y-2); (x-1, y+2); (x-1, y-2)] in
+    let vert_pos = List.flatten (List.map (fun p -> if (in_bound p) && (
+        match piece_captured b p with
+        | Some sth-> sth.team <> pc.team
+        | None -> true
+     ) && ( check_position ((p.y+ y)/2 , x) = None) then [{start= (x,y); destination = p;
+        piece_captured = (check_position b p)}] else []) raw_hori pos
 
-
-  let move_cannon (b:board) (pc:piece) ((x,y): position) :step list =
-    begin
-      let result = ref [] in
-      let flag=ref false in
-      let rec loop_forward curr_position=
-      if !flag=false then
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | None, true -> result := {start = (x,y); destination = curr_position ;
-          piece_captured = None}::(!result); loop_forward (x, y+1)
-         | sth ,true -> flag:=true; loop_forward (x, y+1)
-         | _ , _ -> ()
-      else
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | Some sth ,true -> if sth.team=pc.team then ()
-                             else 
-                    result := {start = (x,y); destination = curr_position;
-                    piece_captured = Some sth}::(!result); 
-         | None, true -> loop_forward (x, y+1)
-         | _ , _ -> ()
-      in
-      let rec loop_back curr_position =
-      if !flag=false then
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | None, true -> result := {start = (x,y); destination = curr_position ;
-          piece_captured = None}::(!result); loop_back (x, y-1)
-         | sth ,true -> flag:=true; loop_back (x, y-1)
-         | _ , _ -> ()
-      else
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | Some sth ,true -> if sth.team=pc.team then ()
-                             else 
-                    result := {start = (x,y); destination = curr_position;
-                    piece_captured = Some sth}::(!result); 
-         | None, true -> loop_back (x, y-1)
-         | _ , _ -> ()
-      in
-      let rec loop_left curr_position =
-      if !flag=false then
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | None, true -> result := {start = (x,y); destination = curr_position ;
-          piece_captured = None}::(!result); loop_left (x-1, y)
-         | sth ,true -> flag:=true; loop_left (x-1, y)
-         | _ , _ -> ()
-      else
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | Some sth ,true -> if sth.team=pc.team then ()
-                             else 
-                    result := {start = (x,y); destination = curr_position;
-                    piece_captured = Some sth}::(!result); 
-         | None, true -> loop_left (x-1, y)
-         | _ , _ -> ()
-      in
-      let rec loop_right curr_position =
-      if !flag=false then
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | None, true -> result := {start = (x,y); destination = curr_position ;
-          piece_captured = None}::(!result); loop_right (x+1, y)
-         | sth ,true -> flag:=true; loop_right (x+1, y)
-         | _ , _ -> ()
-      else
-        match  (check_position b curr_position), (in_bound curr_position) with
-         | Some sth ,true -> if sth.team=pc.team then ()
-                             else 
-                    result := {start = (x,y); destination = curr_position;
-                    piece_captured = Some sth}::(!result); 
-         | None, true -> loop_right (x+1, y)
-         | _ , _ -> ()
-        in
-      let ()=flag:=false in
-      let ()=loop_forward (x, y+1) in
-      let ()=flag:=false in
-      let ()=loop_back (x, y-1) in
-      let ()=flag:=false in
-      let ()=loop_right (x+1 , y) in
-      let ()=flag:=false in
-      let ()=loop_left (x-1, y) in
-      !result
-    end
 
 
   let move_elephant = TODO
-  let move_advisor = TODO
-  let move_general = TODO
+
+
+  let move_advisor (b:board) (pc:piece) ((x,y): position) : step list =
+    let raw_pos = [(x+1, y+1); (x-1, y-1); (x-1, y+1); (x+1, y-1) ] in
+     List.flatten (List.map (fun p -> if (in_bound p) && (
+        match piece_captured b p with
+        | Some sth-> sth.team <> pc.team
+        | None -> true
+     ) && (in_square pc p)then [{start= (x,y); destination = p;
+        piece_captured = (check_position b p)}] else []) raw_pos )
+
+
+  let move_general (b:board) (pc:piece) ((x,y): position) : step list =
+    let raw_pos = [(x+1, y); (x-1, y); (x, y+1); (x, y-1) ] in
+     List.flatten (List.map (fun p -> if (in_bound p) && (
+        match piece_captured b p with
+        | Some sth-> sth.team <> pc.team
+        | None -> true
+     ) && (in_square pc p)then [{start= (x,y); destination = p;
+        piece_captured = (check_position b p)}] else []) raw_pos )
 
 
   let generate_piece_move b pv p = TODO
