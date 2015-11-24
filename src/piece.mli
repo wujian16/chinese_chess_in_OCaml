df4c3ac1b560fc8a2be2b11599a8ed0318e1e0d7
CandiceW <yw654@cornell.edu>
2015-11-23 22:14:18 -0500

    oh..mli!

--- src/piece.mli
+++ src/piece.mli
@@ -9,7 +9,47 @@ type piece={
 name:string;
 print_name : string;
 type_of: piece_info;
-team: bool;
-init_place : position;
-place: position
+team: bool
 }
+val genaralR: piece
+val genaralB: piece
+
+val advisor1R : piece
+val advisor2R : piece
+val advisorB1 : piece
+val advisorB2 : piece
+
+val elepR1 : piece
+val elepR2 : piece
+val elepB1 : piece
+val elepB2 : piece
+
+val horseR1 : piece
+val horseR2 : piece
+val horseB1 : piece
+val horseB2 : piece
+
+val rookR1 : piece
+val rookR2 : piece
+val rookB1 : piece
+val rookB2 : piece
+
+val canR1 : piece
+val canR2 : piece
+val canB1 : piece
+val canB2 : piece
+
+
+val soldR1 : piece
+val soldR2 : piece
+val soldR3 : piece
+val soldR4 : piece
+val soldR5 : piece
+
+val soldB1 : piece
+val soldB2 : piece
+val soldB3 : piece
+val soldB4 : piece
+val soldB5 : piece
+
+
