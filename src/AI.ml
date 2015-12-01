e32b86a819a2be94b01148ccb4f107ff446419b1
Tiffany Li <wl439@cornell.edu>
2015-12-01 00:50:41 -0500

    v0 of best move done; next step needs history table and iterative

--- src/AI.ml
+++ src/AI.ml
@@ -10,14 +10,7 @@ type search_result = {depth:int; best:step list; conclu:int}
 
 type transposition_table = (string, search_result) Hashtbl.t
 
-type history_table = (string, search_result) Hashtbl.
-
-(* Determine AI's color, assuming false is for red in input b *)
-let det_col b : bool = 
-	if b then true else false
-
-(* initialize AI's color to black *)
-let col = true
+type history_table = (string, search_result) Hashtbl.t
 
 let sort = raise TODO
 
@@ -30,6 +23,64 @@ let generate_all_moves (b:board) (p:prev_step) (side:round): step list=
 		 | Some t -> l@t)
 		[] each_steps
 
+
+let alphaBeta (alpha:int) (beta:int) (depth_left:int) 
+	(b:board) (p:prev_step) (ai_col:bool) (curr_rd:bool): (int,step list) =
+
+	if depth_left = 0 then ((evaluate b),[])
+	else
+		if curr_rd = ai_col then
+		
+			let result = ref (int_of_float neg_infinity) in
+			v = ref alpha;
+			i = ref 0;
+			best_steps = ref [];
+			begin match generate_all_moves b p col with
+			| [] -> ((-depth_left) - (int_of_float neg_infinity),[])
+			| l ->
+				let all_moves = Array.of_list l in
+
+				while ((!i < (Array.length all_moves)) && (beta <> !result))
+				do 
+					let (updated_b,updated_prev) = update all_moves.i (b,p) in
+					let (score,new_best_steps) = alphaBeta !v beta (depth_left - 1) 
+								updated_b updated_prev ai_col !curr_rd in
+					if (score > !v) then 
+						v:= score; 
+						best_steps:= all_moves.i::new_best_steps;
+						i:= i+1
+					else if (score > beta) then result := beta; i:= i+1
+					else i:= i+1
+				done;
+				if !result = beta then (beta,!best_steps) else (!v,!best_steps)
+			end
+
+		else
+
+			let result = ref (int_of_float infinity) in
+			v = ref beta;
+			i = ref 0;
+			best_steps = ref [];
+			begin match generate_all_moves b p col with
+			| [] -> (depth_left - (int_of_float infinity),[])
+			| l ->
+				let all_moves = Array.of_list l in
+				while ((!i < (Array.length all_moves)) && (alpha <> !result))
+				do 
+					let (updated_b,updated_prev) = update all_moves.i (b,p) in
+					let (score,new_best_steps) = alphaBeta alpha !v (depth_left - 1) 
+								updated_b updated_prev ai_col !curr_rd in
+					if (score < !v) then 
+						v:= score; 
+						best_steps:= all_moves.i::new_best_steps;
+						i:= i+1
+					else if (score < alpha) then result := alpha; i:= i+1
+					else i:= i+1
+				done;
+				if !result = alpha then (alpha,!best_steps) else (!v,!best_steps)
+			end
+
+(*
 let alphaBetaMax (alpha:int ref) (beta:int ref) (depth_left:int) 
 	(b:board) (p:prev_step): int = 
 	
@@ -38,10 +89,11 @@ let alphaBetaMax (alpha:int ref) (beta:int ref) (depth_left:int)
 	else 
 		let all_moves = Array.of_list (generate_all_moves b p col) in
 		let i = ref 0 in
-		while ((!i <= (Array.length all_moves)) && (!beta <> !result)) 
+		while ((!i < (Array.length all_moves)) && (!beta <> !result)) 
 		do (* need to update board for each move...... *)
-			let updated_b = 
-			let score = alphaBetaMin alpha beta (depth_left - 1) in
+			let (updated_b,updated_prev) = update all_moves.i (b,p) in
+			let score = alphaBetaMin alpha beta (depth_left - 1) 
+						updated_b updated_prev in
 			if (score >= !beta) then result := !beta; i:=!i+1
 			else if (score > !alpha) then alpha := score; i:=!i+1
 			else i:=!i+1
@@ -56,16 +108,21 @@ and alphaBetaMin (alpha:int ref) (beta:int ref) (depth_left:int)
 	else
 		let all_moves = Array.of_list (generate_all_moves b p !col) in
 		let i = ref 0 in
-		while ((!i <= (Array.length all_moves)) && (!alpha <> !result)) 
+		while ((!i < (Array.length all_moves)) && (!alpha <> !result)) 
 		do
-			let score = alphaBetaMax alpha beta (depth_left - 1) in
+			let (updated_b,updated_prev) = update all_moves.i (b,p) in
+			let score = alphaBetaMax alpha beta (depth_left - 1) 
+						updated_b updated_prev in
 			if (score <= !alpha) then result := !alpha; i:=!i+1
 			else if (score < !beta) then beta := score; i:=!i+1
 			else i:=!i+1
 		done;
 		if !result = !alpha then !alpha else !beta
-
-let best_move_v0 (n:int) (b:board) : step list = 
+*)
+let best_move_v0 (n:int) (b:board) (p:step list): step list = 
 	(* need to modify the alpha beta to keep track of the optimum steps *)
-	raise TODO
+	let (score, next_steps) = 
+		alphaBeta (int_of_float neg_infinity) (int_of_float infinity) 
+			n b p col round in
+	next_steps
 
--- src/AI.mli
+++ src/AI.mli
@@ -15,17 +15,11 @@ type transposition_table
 (* record history AI computations*)
 type history_table
 
-(* Determine AI's side color *)
-val det_col: bool -> bool
-
-(* The color of AI's side; Red is true and Black if false *)
-val col: bool
-
 (* sort the all possible moves*)
 val sort: board-> step list
 
 (* generate the best move using non-iterative alpha beta w/o tables *)
-val best_move_v0: int-> board-> step list
+val best_move_v0: int-> board-> step list -> step list
 
 (*generate the best possible moves for the futural several steps
 * int below is how many steps we predict for the future*)
