c3dcd95dadf25809b0e86e86a367a00f2f2042ae
Tiffany Li <wl439@cornell.edu>
2015-12-05 17:49:47 -0500

    test ver of one recursive call in AI

--- src/AI.ml
+++ src/AI.ml
@@ -171,57 +171,98 @@ let rec alphaBeta (alpha:int) (beta:int) (depth_left:int)
 		)
 
 
+let rec alphaBeta2 (alpha:int) (beta:int) (depth_left:int)
+	(b:board) (p:prev_step) (ai_col:bool) (curr_rd:bool): int*step list =
+
+	Printf.printf "\nThe depth is:";
+	print_int depth_left;
+	if depth_left = 0 then (cnt := !cnt + 1; Printf.printf "I am at depth 0";print_int (eval_board b curr_rd); print_endline " ";
+			print_int (eval_board b (not curr_rd)); print_endline " ";(*print_int !cnt; Printf.printf "\n"; *)((eval_board b curr_rd),[]))
+	else if check_end_game b p then (cnt := !cnt + 1; (*print_int !cnt; Printf.printf "\n";*)((eval_board b curr_rd),[]))
+	else
+	(
+			let result =
+			(if ai_col = curr_rd then ref min_int
+			else ref max_int)
+			in
+			let v = ref alpha in
+			let i = ref 0 in
+			let best_steps = ref [] in
+			(*begin match (generate_all_moves b p curr_rd) with*)
+			let h_e = 
+			(if ai_col = curr_rd then (generate_piece_move b p canR1)
+			else (generate_piece_move b p canB1)
+				) in
+			begin match h_e with
+			| [] -> (depth_left - max_int,[])
+			| l ->
+				List.iter (fun a -> if (in_bound a.start)&&(in_bound a.destination) then ()
+					else print_step a) l;
+				(*
+				let sorted_l = List.sort compared l in*)
+				let all_moves = Array.of_list l in
+
+				while ((!i < (Array.length all_moves)) && (beta <> !result))
+				do
+
+					let (updated_b,updated_prev) = update_unmutable all_moves.(!i) b p in
+					let (neg_score,new_best_steps) = 
+						(alphaBeta2 (-beta) (-(!v)) (depth_left - 1)
+						updated_b updated_prev ai_col (not curr_rd)) in
+					let score = (-neg_score) in
+					
+					Printf.printf "\nIts %d's child has score %d. Its best moves are: \n" !i score;
+					List.iter print_step new_best_steps;
+					Printf.printf "\nThe current alpha is %d; beta is %d\n" !v beta;
+          			(*let flag=ref false in*)
+					(if (score > !v) then
+						(
+						v:= score;
+						Printf.printf "Score is greater than current alpha. Alpha is updated: %d\n" !v;
+ 						best_steps:= all_moves.(!i)::new_best_steps;
+						(*flag:=true*)
+						Printf.printf "Now the best steps are: \n";
+						List.iter print_step !best_steps
+						)
+					else if (score > beta) then
+						(Printf.printf "Score is greater than beta, impossible, break immediately\n";
+						result := beta
+					  (*flag:=true*)
+					  )
+					else (Printf.printf "Nothing happens\n")
+					);
 (*
-let alphaBetaMax (alpha:int ref) (beta:int ref) (depth_left:int)
-	(b:board) (p:prev_step): int =
+					(if (!flag=true) then
+						let new_val=(Hashtbl.find nHistoryTable
+						(all_moves.(!i).start,all_moves.(!i).destination))+(depth_left*depth_left)
+						in
+						(Hashtbl.replace nHistoryTable
+						(all_moves.(!i).start,all_moves.(!i).destination)
+					  new_val)
+          			else ());*)
+					cnt := !cnt + 1;
+					i:= !i+1
+				done;
+				(*print_int !cnt;Printf.printf "\n";*)
+				if !result = beta then (Printf.printf "Break with beta %d; Best steps are: \n" beta;
+										List.iter print_step !best_steps; (beta,!best_steps))
+				else (Printf.printf "Node's value is %d; Best steps are: \n" !v;
+					List.iter print_step !best_steps; (!v,!best_steps))
+			end
+)
 
-	let result = ref (int_of_float neg_infinity) in
-	if depth_left = 0 then (evaluate b)
-	else
-		let all_moves = Array.of_list (generate_all_moves b p col) in
-		let i = ref 0 in
-		while ((!i < (Array.length all_moves)) && (!beta <> !result))
-		do (* need to update board for each move...... *)
-			let (updated_b,updated_prev) = update all_moves.i (b,p) in
-			let score = alphaBetaMin alpha beta (depth_left - 1)
-						updated_b updated_prev in
-			if (score >= !beta) then result := !beta; i:=!i+1
-			else if (score > !alpha) then alpha := score; i:=!i+1
-			else i:=!i+1
-		done;
-		if !result = !beta then !beta else !alpha
-
-and alphaBetaMin (alpha:int ref) (beta:int ref) (depth_left:int)
-	(b:board) (p:prev_step): int =
-
-	let result = ref (int_of_float infinity) in
-	if depth_left = 0 then -(evaluate b)
-	else
-		let all_moves = Array.of_list (generate_all_moves b p !col) in
-		let i = ref 0 in
-		while ((!i < (Array.length all_moves)) && (!alpha <> !result))
-		do
-			let (updated_b,updated_prev) = update all_moves.i (b,p) in
-			let score = alphaBetaMax alpha beta (depth_left - 1)
-						updated_b updated_prev in
-			if (score <= !alpha) then result := !alpha; i:=!i+1
-			else if (score < !beta) then beta := score; i:=!i+1
-			else i:=!i+1
-		done;
-		if !result = !alpha then !alpha else !beta
-*)
 
 let best_move_v0 (n:int) (b:board) (p:prev_step) (ai_col:bool): int*step list =
 	(* need to modify the alpha beta to keep track of the optimum steps *)
-	  let t=Unix.gettimeofday () in
+	  (*let t=Unix.gettimeofday () in
 		let result=ref (0, []) in
 		let i=ref 1 in
-		while ((Unix.gettimeofday () -. t)<=300.0 && !i<=n) do
-			result:=(alphaBeta min_int max_int (!i) b p ai_col ai_col);
-			i:=!i+1
+		while ((Unix.gettimeofday () -. t)<=300.0 && !i<=n) do*)
+			(*result:=*)(alphaBeta2 min_int max_int n b p ai_col ai_col)
+			(*i:=!i+1
 		done;
 		
-		!result
+		!result*)
 
 (*generate the best possible moves for the futural several steps
 * int below is how many steps we predict for the future*)
